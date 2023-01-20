0554fe193435e6498334028f5f151a12b787d5a8
tatotime <96212135+tatotime@users.noreply.github.com>
2023-01-19 23:38:59 -0500

    Create App.java

---
+++ App.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        FileTasks fileEd = new FileTasks();
+        File file = new File("nameHolder.txt");
+        if (file.isFile()){
+            System.out.println("Name file exists");
+        } else {
+            fileEd.CreateFile("nameHolder.txt");
+        }
+        fileEd.ReadNameFile();
+}
+}
