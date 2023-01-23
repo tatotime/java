c46e16a3e0a8fc2238bb21559666e52589defa92
tatotime <96212135+tatotime@users.noreply.github.com>
2023-01-23 13:58:38 -0500

    Update App.java

--- App.java
+++ App.java
@@ -1,14 +1,15 @@
 import java.io.File;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        FileTasks fileEd = new FileTasks();
-        File file = new File("nameHolder.txt");
-        if (file.isFile()){
-            System.out.println("Name file exists");
-        } else {
-            fileEd.CreateFile("nameHolder.txt");
-        }
-        fileEd.ReadNameFile();
-}
+         public static void main(String[] args) throws Exception {
+            FileTasks fileEd = new FileTasks();
+            File file = new File("nameHolder.txt");
+            if (file.isFile()){
+                 System.out.println("Name file exists");
+            } else {
+               fileEd.CreateFile("nameHolder.txt");
+               }
+            fileEd.enterName();
+            fileEd.nameCheck();
+         }
 }
