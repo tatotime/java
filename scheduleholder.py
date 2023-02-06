d983253723fffad5d9c2737011307d7ce5bbfcfe
tatotime <96212135+tatotime@users.noreply.github.com>
2023-02-05 20:27:00 -0500

    Update scheduleholder.py

--- scheduleholder.py
+++ scheduleholder.py
@@ -4,17 +4,17 @@ f.close()
 print("Hello! This is my Schedule Manager v1. \nWhat would you like to do?")
 tempInput = input("Check, Update, or Clear: ")
 nInput = tempInput.lower()
-if nInput == "check":
+if nInput.equals("check"):
     f = open("Schedule.txt", "r")
     f.read()
     f.close()
-elif nInput == "update":
+elif nInput.equals("update"):
     print("Input your schedule for the week")
     f = open("Schedule.txt", "a")
     week = input()
     f.write(week)
     f.close()
-elif nInput == "clear":
+elif nInput.equals("clear"):
     print("Schedule has been cleared.")
     f = open("Schedule.txt", "w")
     f.write(" ")
