27c8b86638d143c45891d666e2704e5be8efa6df
tatotime <96212135+tatotime@users.noreply.github.com>
2023-02-08 13:15:54 -0500

    Add files via upload

---
+++ GamePanel.java
@@ -0,0 +1,95 @@
+package main;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+
+import java.awt.Color;
+
+public class GamePanel extends JPanel implements Runnable{
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
+    // FPS limit
+    int FPS = 60;
+
+    final int tileSize = originalTileSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 px
+    final int screenHeight = tileSize * maxScreenRow; // 576 px
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+
+    // set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+
+    }
+
+    public void startGameThread(){
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    @Override
+    public void run() {
+        double drawInterval = 1000000000/FPS; // 0.016666 seconds
+        double nextDrawTime = System.nanoTime() + drawInterval;
+        
+        while (gameThread != null){
+
+            update();
+
+            repaint();
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000;
+
+                if (remainingTime <0){
+                    remainingTime = 0;
+                }
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+            } catch (InterruptedException e){
+                e.printStackTrace();
+                
+            }
+
+        }
+    }
+    public void update() {
+        if(keyH.upPressed == true){
+            playerY -= playerSpeed;
+
+        } else if (keyH.downPressed == true){
+            playerY += playerSpeed;
+        }else if (keyH.leftPressed == true){
+            playerX -= playerSpeed;
+        }else if (keyH.rightPressed == true){
+            playerX += playerSpeed;
+        }
+    }
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
+    }
+}
--- Main.java
+++ Main.java
@@ -1,85 +1,23 @@
+package main;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
-
+import javax.swing.JFrame;
 
 public class Main {
-    boolean verify;
-    String userName;
-    
-    public void UserCheck(){
-        Scanner userChecker = new Scanner(System.in);
-        System.out.println("Enter Name:");
 
-        String userName = userChecker.nextLine();
-        System.out.println(userName);
-        this.ReadNameFile();
-        if (f.exists()){
-            
-        }
-        this.WriteToName();
-        userChecker.close();
+    public static void main(String args[]){
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Funtimes");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        gamePanel.startGameThread();
     }
-    public void VerifyStatus(boolean verify){
-        System.out.println("Verification Status: "+ verify);
-    }
-    public void CreateNameFile(){
-        File f = new File("nameHolder.txt");
-        try {
-            if (f.createNewFile()){
-            System.out.println("File Created: " + f.getName());
-            } else {
-                System.out.println("File already exists.");
-            }
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
-    public void ReadNameFile(){
-        try {
-            File f = new File("nameHolder.txt");
-            Scanner Reader = new Scanner(f);
-            while (Reader.hasNextLine()){
-                String data = Reader.nextLine();
-                if (data == userName){
-                    System.out.println("Name Verified");
-                    verify = true;
-                    break;
-                }
-            if (verify != true){
-                System.out.println("New name detected, would u like to create a new schedule?");
-            }
-            }
-        Reader.close();
-        } catch (IOException e) {
-            System.out.println("The file could not be read.");
-            e.printStackTrace();
-        }
-    }
-    public boolean isVerify() {
-        return verify;
-    }
-    public void setVerify(boolean verify) {
-        this.verify = verify;
-    }
-    public void WriteToName(){
-        try {
-            FileWriter writer = new FileWriter("nameHolder.txt");
-            writer.write(userName);
-            System.out.println(userName + "Has been added to the file.");
-            writer.close();
-        } catch (IOException e){
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
-    public static void main(String[] args){
-        Main newRun = new Main();
-        newRun.UserCheck();
-        newRun.CreateNameFile();
-    }
+    
 }
-// An Attribute is a variable of a class
